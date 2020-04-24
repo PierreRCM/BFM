8ddcbe3747c627541906dc3a389b995e2546628d
fpedaci <fpedaci@gmail.com>
2020-04-24 16:31:21 +0200

    errors dkin dkout added

--- statorBinding_model/dwelltimes.py
+++ statorBinding_model/dwelltimes.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 
 from LangmuirSimple import LangmuirSimple
 from TwoStateCatchTrad import TwoStateCatchTrad
@@ -101,16 +102,50 @@ def find_jumps_intime(d, n_win=20, last='max', kin=None, kout=None, N0=None, plo
         # find N_nm in window:
         tmn_widx = np.nonzero(wi < t_mn*(t_mn <= wi+w_dt))[0]
         Nmn_in_win[l] = np.mean(N_mn[tmn_widx]) if len(N_mn[tmn_widx]) else None
-
+    
+    # PLOTS:
     if plots:
+        
+        def njumps_theo(kin, kout, Nmn, w_dt):
+            '''theoretical n.jumps in win from LangmuirSimple'''
+            return (kin*(Nmax-Nmn) + kout*Nmn)*w_dt
+        
+        def maxmin_LangTheo(kin0, dkin, kout0, dkout, N, plots=False):
+            ''' return max and min of njumps_theo() and langmuir_exp()
+            given rates (kin0,kout0) and rate error (dkin,dkout) '''
+            if plots:
+                fig = plt.figure('maxmin_LangTheo', clear=True)
+                ax1 = fig.add_subplot(311)
+                ax2 = fig.add_subplot(312)
+                ax3 = fig.add_subplot(313)
+            for i,(kin,kout) in enumerate(itertools.product((kin0-dkin, kin0+dkin), (kout0-dkout, kout0+dkout))):
+                if i==0:
+                    njumps_theo_max = njumps_theo(kin, kout, N, w_dt)
+                    njumps_theo_min = njumps_theo(kin, kout, N, w_dt)
+                    N_theo_min = langmuir_exp(t_mn, kin, kout, N0)
+                    N_theo_max = langmuir_exp(t_mn, kin, kout, N0)
+                njumps_theo_max = np.max([njumps_theo_max, njumps_theo(kin, kout, N, w_dt)], axis=0)
+                njumps_theo_min = np.min([njumps_theo_min, njumps_theo(kin, kout, N, w_dt)], axis=0)
+                N_theo_max      = np.max([N_theo_max, langmuir_exp(t_mn, kin, kout, N0)], axis=0)
+                N_theo_min      = np.min([N_theo_min, langmuir_exp(t_mn, kin, kout, N0)], axis=0)
+                if plots:
+                    ax1.plot(njumps_theo(kin, kout, N, w_dt), label=f'{kin:.2f} {kout:.2f}')
+                    ax2.plot(njumps_theo_max)
+                    ax3.plot(njumps_theo_min)
+                    ax1.legend(fontsize=9)
+            return njumps_theo_max, njumps_theo_min, N_theo_max, N_theo_min
+        
         fig = plt.figure('find_jumps_intime', clear=1)
         ax1 = fig.add_subplot(221)
         ax2 = fig.add_subplot(222, sharex=ax1)
         ax3 = fig.add_subplot(223, sharex=ax1)
         ax4 = fig.add_subplot(224)
         for k in d:
+            # all traces:
             ax1.plot(d[k]['time'][::10], d[k]['N'][::10], alpha=0.4)
+        # dots for jumps in traces:
         ax1.plot(jump_times, np.zeros(len(jump_times)), 'o', alpha=0.2)
+        # mean trace:
         ax1.plot(t_mn, N_mn, 'k', lw=2, label='mean')
         for w in w_ts:
             # plot windows:
@@ -119,25 +154,37 @@ def find_jumps_intime(d, n_win=20, last='max', kin=None, kout=None, N0=None, plo
             ax3.axvline(w, alpha=0.2)
         ax2.plot(w_ts+w_dt/2, jumps_in_win, 'o', label='jumps in win')
         ax2.plot(w_ts+w_dt/2, traces_in_win, 's', alpha=0.6, label='traces in win')
-        ax3.plot(w_ts+w_dt/2, jumps_over_traces, 'ko', label='data') #TODO error?
-        ax4.plot(Nmn_in_win, jumps_over_traces, 'ko', alpha=0.4, label='data')
-        ax4.plot(Nmn_in_win, jumps_over_traces, '-', alpha=0.2)
-        if kin != None and kout != None:
+        ax3.plot(w_ts+w_dt/2, jumps_over_traces, 'ko', label='data') #TODO error on points?
+        ax4.plot(Nmn_in_win, jumps_over_traces, '-ko', alpha=0.5, label='data')
+
+        # if kin +- dkin provided:
+        if kin != None and kout != None and N0 != None:
+            if type(kin)==tuple and type(kout)==tuple:
+                kin0, dkin  = kin
+                kout0, dkout = kout
+                njmax, njmin, Nthmax, Nthmin = maxmin_LangTheo(kin0, dkin, kout0, dkout, Nmn_in_win, plots=0)
+                # LangmuirSimple error band:
+                ax3.fill_between(w_ts+w_dt/2, njmax, njmin, color='r', alpha=0.7, linewidth=0) 
+            elif type(kin)==float and type(kout)==float:
+                kin0, kout0 = kin, kout
             # theoretical numb. of jumps in win for LangmuirSimple:
-            ax3.plot(w_ts+w_dt/2, (kin*(Nmax-Nmn_in_win) + kout*Nmn_in_win)*w_dt, 'r--', label='Lang.Theo')
-            ax4.plot(np.arange(Nmax+1), (kin*(Nmax-np.arange(Nmax+1)) + kout*np.arange(Nmax+1))*w_dt, 'r--', label='Lang.Theo')
-            if N0 != None:
-                lab = f'Lang.Theo\n(N0:{N0} kin:{kin}\nkout:{kout})'
-                ax1.plot(t_mn, langmuir_exp(t_mn, kin, kout, N0), 'r--', label=lab)
+            ax3.plot(w_ts+w_dt/2, njumps_theo(kin0, kout0, Nmn_in_win, w_dt), 'r--', label='Lang.Theo')
+            ax4.plot(np.arange(Nmax+1), (kin0*(Nmax-np.arange(Nmax+1)) + kout0*np.arange(Nmax+1))*w_dt, 'r--', label='Lang.Theo')
+            ax4.fill_between(Nmn_in_win, njmin, njmax, linewidth=0, color='r', alpha=0.4)
+            # LangmuirSimple error band:
+            lab = f'Lang.Theo N0:{N0}\nkin:{kin0}$\pm{dkin}$\nkout:{kout0}$\pm${dkout}'
+            ax1.fill_between(t_mn, Nthmin, Nthmax, color='r', alpha=0.9, linewidth=0)
+            ax1.plot(t_mn, langmuir_exp(t_mn, kin0, kout0, N0), 'r--', label=lab)
+
         ax1.set_ylabel('N(t)')
         ax3.set_ylabel('<jumps> in win.')
         ax1.grid(False)
         ax2.grid(False)
         ax3.grid(False)
-        ax1.legend(fontsize=8)
-        ax2.legend(fontsize=8)
-        ax3.legend(fontsize=8)
-        ax4.legend(fontsize=8)
+        ax1.legend(fontsize=7)
+        ax2.legend(fontsize=7)
+        ax3.legend(fontsize=7)
+        ax4.legend(fontsize=7)
         ax1.set_xlabel('time (s)')
         ax2.set_xlabel('time (s)')
         ax3.set_xlabel('time (s)')
@@ -163,7 +210,6 @@ def find_mean_trace(d, last='min', plots=False):
         idx = np.max([len(i['time']) for i in list(dd.values())])
         m = last if type(last)==float else 1
         idx = int(idx*m)
-        print(idx)
         for i in dd:
             dif = idx-len(dd[i]['N'])
             if dif > 0:
